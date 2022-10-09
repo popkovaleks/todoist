0f3f6d97a2fdf2b63a3073657643deaabec36c39
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-09 22:50:07 +0500

    add and update methods

--- todoist/todo/api/v1/views.py
+++ todoist/todo/api/v1/views.py
@@ -12,3 +12,32 @@ class TaskViewset(viewsets.ViewSet):
         if tasks:
             return Response(data=serializers.TaskSerializer(tasks, many=True).data, status=status.HTTP_200_OK)
         return Response('No tasks found', status=status.HTTP_404_NOT_FOUND)
+
+
+    def add(self, request):
+        data = request.data
+        serializer = serializers.TaskSerializer(data=data)
+
+        if serializer.is_valid():
+            models.Task.objects.create(
+                title=serializer.data.get('title'),
+                content=serializer.data.get('content'),
+                done=serializer.data.get('done')
+            )
+            return Response(data=data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def update(self, request):
+        data = request.data
+        serializer = serializers.TaskSerializer(data=data)
+
+        if serializer.is_valid():
+            models.Task.objects.get(id=serializer.data.get('id'))\
+                .update(
+                    title=serializer.data.get('title'),
+                    content=serializer.data.get('content'),
+                    done=serializer.data.get('done')
+                )
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
