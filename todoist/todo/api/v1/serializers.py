40c659b3b99d59e05648632702b369132ae5d663
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-11 16:20:33 +0500

    create and update methods in serializer

--- todoist/todo/api/v1/serializers.py
+++ todoist/todo/api/v1/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
+from todo.models import Task
+
 class TaskSerializer(serializers.Serializer):
     id = serializers.IntegerField(
         read_only=True
     )
-    
+
     title = serializers.CharField(
         required = True
     )
@@ -16,3 +18,13 @@ class TaskSerializer(serializers.Serializer):
     done = serializers.BooleanField(
         required=True
     )
+
+    def create(self, validated_data):
+        return Task.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.done = validated_data.get('done', instance.done)
+        instance.save()
+        return instance
