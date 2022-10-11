b094b9a6b318cdd80b146b485efd96f05c174f54
Aleksandr <popkov.aleks.v@yandex.ru>
2022-10-11 16:21:50 +0500

    delete and details methods, add and update now using setializer

--- todoist/todo/api/v1/views.py
+++ todoist/todo/api/v1/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from todo.api.v1 import serializers
 from todo import models
@@ -7,7 +8,7 @@ from todo import models
 class TaskViewset(viewsets.ViewSet):
 
     def list(self, request):
-        tasks = models.Task.objects.all()
+        tasks = models.Task.objects.all().values('id','title','done')
 
         if tasks:
             return Response(data=serializers.TaskSerializer(tasks, many=True).data, status=status.HTTP_200_OK)
@@ -19,25 +20,35 @@ class TaskViewset(viewsets.ViewSet):
         serializer = serializers.TaskSerializer(data=data)
 
         if serializer.is_valid():
-            models.Task.objects.create(
-                title=serializer.data.get('title'),
-                content=serializer.data.get('content'),
-                done=serializer.data.get('done')
-            )
+            serializer.save()
             return Response(data=data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     
     def update(self, request):
         data = request.data
-        serializer = serializers.TaskSerializer(data=data)
-
+        task = models.Task.objects.get(id=data.get('id'))
+        serializer = serializers.TaskSerializer(task, data=data)
+        
         if serializer.is_valid():
-            models.Task.objects.get(id=serializer.data.get('id'))\
-                .update(
-                    title=serializer.data.get('title'),
-                    content=serializer.data.get('content'),
-                    done=serializer.data.get('done')
-                )
+            serializer.save()
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request):
+        data = request.data
+        models.Task.objects.get(id=data.get('id')).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TaskDetailViewset(viewsets.ViewSet):
+
+    def details(self, request, pk):
+        task = models.Task.objects.get(id=pk)
+        serializer = serializers.TaskSerializer(data=task.__dict__)
+
+        if serializer.is_valid():
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+        
